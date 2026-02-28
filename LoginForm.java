bb51073c0edc85352ae3235898d6397de7f879d5
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-02-28 20:47:22 +0545

    login form

---
+++ LoginForm.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LoginForm {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Login Form");
+
+        JLabel userLabel = new JLabel("Username:");
+        JLabel passLabel = new JLabel("Password:");
+        JTextField userText = new JTextField();
+        JPasswordField passText = new JPasswordField();
+        JButton loginButton = new JButton("Login");
+
+        userLabel.setBounds(50, 30, 100, 30);
+        userText.setBounds(150, 30, 150, 30);
+        passLabel.setBounds(50, 70, 100, 30);
+        passText.setBounds(150, 70, 150, 30);
+        loginButton.setBounds(150, 110, 100, 30);
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = userText.getText();
+                String password = new String(passText.getPassword());
+
+                if(username.equals("admin") && password.equals("1234")) {
+                    JOptionPane.showMessageDialog(frame, "Login Successful!");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Invalid Credentials");
+                }
+            }
+        });
+
+        frame.add(userLabel);
+        frame.add(userText);
+        frame.add(passLabel);
+        frame.add(passText);
+        frame.add(loginButton);
+
+        frame.setSize(400, 250);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
